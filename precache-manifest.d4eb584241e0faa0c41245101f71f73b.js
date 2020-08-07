133f4bd9925ea15c9c7496d46e31255570099465
Aashray Shrestha <aashray.138@gmail.com>
2020-08-07 09:33:48 -0400

    Updates

---
+++ precache-manifest.d4eb584241e0faa0c41245101f71f73b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337d7cbb1ec9f0facbc5cc5257b3442c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e59e5a3e6113e5c872e3",
+    "url": "/my-app/static/css/2.0cee8fd6.chunk.css"
+  },
+  {
+    "revision": "fbeb6eea392f94cd88c2",
+    "url": "/my-app/static/css/main.8eec4104.chunk.css"
+  },
+  {
+    "revision": "e59e5a3e6113e5c872e3",
+    "url": "/my-app/static/js/2.f188cedc.chunk.js"
+  },
+  {
+    "revision": "32ad5e7eab35d7758a64b4509f4a87ec",
+    "url": "/my-app/static/js/2.f188cedc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbeb6eea392f94cd88c2",
+    "url": "/my-app/static/js/main.26c6d587.chunk.js"
+  },
+  {
+    "revision": "1840cf459b4a02ccf7d6",
+    "url": "/my-app/static/js/runtime-main.31abddd6.js"
+  },
+  {
+    "revision": "c8712b4736c2669afee3aea2687a683f",
+    "url": "/my-app/static/media/Artville.c8712b47.png"
+  },
+  {
+    "revision": "ced880680f9a06c6db5d29eaa9e00bab",
+    "url": "/my-app/static/media/CampusCalendar.ced88068.png"
+  },
+  {
+    "revision": "ca9d5efd734a4c14658e7bf8c3687621",
+    "url": "/my-app/static/media/LightenUp.ca9d5efd.png"
+  },
+  {
+    "revision": "3de480046efa07eb54ff99a2b88d1851",
+    "url": "/my-app/static/media/ResumeBuilder.3de48004.png"
+  },
+  {
+    "revision": "ad4da4cc127fbdbe9e3edbeeeeb8a67a",
+    "url": "/my-app/static/media/Transcript.ad4da4cc.png"
+  },
+  {
+    "revision": "2158bea983a036ae0eec3b8eebc40a8c",
+    "url": "/my-app/static/media/WhereUAtt.2158bea9.png"
+  },
+  {
+    "revision": "68ca7024341abd2b3f907b3e80908654",
+    "url": "/my-app/static/media/aashray.68ca7024.jpg"
+  }
+]);
